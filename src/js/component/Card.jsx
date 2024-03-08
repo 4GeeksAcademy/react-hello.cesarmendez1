3c5a16bf287d7ca7d16ca914039e26617169b582
CesarMendez1 <152318003+CesarMendez1@users.noreply.github.com>
2024-03-08 21:54:00 +0000

    trabajo terminado pero debe ser revisado por astrid

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -6,19 +6,19 @@ import { ProgressPlugin } from "webpack";
 
 
 //create your first component
-const Card  = (props) => {
-	return (
-		<div>
-			<div class="card" style={{width: "18rem"}}>
-  <img src={props.imagen} class="card-img-top" alt="..."/>
-  <div class="card-body">
-    <h5 class="card-title">{props.title}</h5>
-    <p class="card-text">{props.parrafo}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-		</div>
-	);
+const Card = (props) => {
+  return (
+    <div>
+      <div class="card" style={{ width: "18rem" }}>
+        <img src={props.imagen} class="card-img-top" alt="..." />
+        <div class="card-body">
+          <h5 class="card-title">{props.title}</h5>
+          <p class="card-text">{props.parrafo}</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,19 +4,30 @@ import Navbar from "./Navbar.jsx";
 import Card from "./Card.jsx";
 import Jumbotron from "./Jumbotron.jsx";
 import Footer from "./Footer.jsx";
-const Card = [
-{imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUab3p1h5qGvJ-_GSwcuEnaf2Ht9VIUem4z9uOS09NGw&s",title="carta1", parrafo="parrafo1"},
-{imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIDADUKCsdEclREq2RGW7ABYTi5Q2y36jB7B34wngx1A&s",title="carta2", parrafo="parrafo2"},
-{imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2hhP8qLJ5aBH4RynhJHXnMuqI7E6-1teaBKrPjgarIQ&s",title="carta3", parrafo="parrafo3"},
-{imagen:"https://pbs.twimg.com/profile_images/1188507013233479681/WuNwaQ8R_400x400.jpg", title="carta4", parrafo="parrafo4"}
-]
+
 //create your first component
 const Home = () => {
+	const card = [
+		{imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUab3p1h5qGvJ-_GSwcuEnaf2Ht9VIUem4z9uOS09NGw&s",title:"carta1", parrafo:"parrafo1"},
+		{imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIDADUKCsdEclREq2RGW7ABYTi5Q2y36jB7B34wngx1A&s",title:"carta2", parrafo:"parrafo2"},
+		{imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2hhP8qLJ5aBH4RynhJHXnMuqI7E6-1teaBKrPjgarIQ&s",title:"carta3", parrafo:"parrafo3"},
+		{imagen:"https://pbs.twimg.com/profile_images/1188507013233479681/WuNwaQ8R_400x400.jpg", title:"carta4", parrafo:"parrafo4"}
+		];
 	return (
 		<div>
 			<Navbar/>
 			<Jumbotron/>
-			<Card/>
+			<div>
+				<div>
+					{card.map((item,index)=>{
+						return(
+							<Card key={index} imagen={item.imagen} title={item.title} parrafo={item.parrafo}/>
+						)
+					})}
+				
+				</div>
+			</div>
+			
 			<Footer/>
 		</div>
 	);
